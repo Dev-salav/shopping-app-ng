d0c42ae6050dd3667aa3f96c5a64637461e810a3
Dev-salav <de.salav24@gmail.com>
2022-11-30 09:38:07 -0500

    sole commit

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { AlertComponent } from './alert/alert.component';
+import { DropdownDirective } from './dropdown.directive';
+import { PlaceholderDirective } from './placeholder/placerholder.directive';
+
+@NgModule({
+  declarations: [AlertComponent, PlaceholderDirective, DropdownDirective],
+  imports: [CommonModule],
+  exports: [
+    AlertComponent,
+    PlaceholderDirective,
+    DropdownDirective,
+    CommonModule,
+  ],
+})
+export class SharedModule {}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseAPIKey: 'AIzaSyDu4w8Ho_T3MWUNpnoTWbSquJ5ymtoIObc',
 };
